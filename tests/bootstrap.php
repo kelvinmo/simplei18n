7f42ecc758791de76109a266d1054b7777ad5c03
Kelvin Mo <kmo@users.sourceforge.net>
2015-09-12 09:20:26 +1000

    Changed classloader to PSR-4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,16 +9,17 @@ set_include_path(implode(PATH_SEPARATOR, array(
 chdir(dirname(__FILE__) . '/');
 
 spl_autoload_register(function ($class) {
-    $class = ltrim($class, '\\');
+    $prefix = 'SimpleI18N\\';
+    $base_dir = __DIR__ . '/../src/';
 
-    if (strncmp('SimpleI18N\\', $class, 11) === 0) {
-        $relative_class = substr($class, 11);
-        $file = dirname(__FILE__) . '/../src/' . $relative_class . '.php';
-        if (file_exists($file)) {
-            include_once $file;
-        }
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) return;
+
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        include_once $file;
     }
-    return;
 });
 
 ?>
